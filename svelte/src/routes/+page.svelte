dc6d68261be35e7afcaa9aa85ce4bfef8353ec8a
jbugel-lol <jonasbugelnig+gh1@jbugel.xyz>
2024-03-14 09:17:28 +0100

    dark reader detector

---
+++ svelte/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    const darkReaderMetaTag = document.querySelector('meta[name="darkreader"]');
+    if (darkReaderMetaTag) {
+      alert('You\'re using darkreader, which breaks the styles on this page. Please disable it. :)');
+    }
+  });
+</script>
+
+
