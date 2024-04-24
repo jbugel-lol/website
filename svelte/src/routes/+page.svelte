a81e04583a2d1d3612ff3dc52a3b370d8f5786ed
jbugel <115107409+jbugel-lol@users.noreply.github.com>
2024-04-24 21:39:35 +0200

    some redirects

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,12 +1,31 @@
-<script>
-  import { onMount } from 'svelte';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="JBugel's Website">
+  <meta property="og:url" content="https://jbugel.xyz">
+  <meta property="og:description" content="Discover my projects, profile and more">
+  <meta property="og:image" content="https://example.com/images/website-preview.png">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@jbugel">
+  <meta name="twitter:creator" content="@jbugel">
+  <meta name="twitter:title" content="JBugel's Website">
+  <meta name="twitter:description" content="Discover my projects, profile and more">
+  <meta name="twitter:image" content="https://example.com/images/website-preview.png">
+  <title>jbugel</title>
+</head>
+<body class="h-screen w-screen bg-cat-base">
+<a class="text-cat-text flex items-center" href="/projects">
+  <img src="/file-folder.png" alt="Headphones" class="h-6 mr-2">Projects
+</a><a class="text-cat-text flex items-center" href="/contact">
+  <img src="/envelope.png" alt="Headphones" class="h-6 mr-2">Contact Me
+</a>
+<a class="text-cat-text flex items-center" href="/music">
+  <img src="/headphones.png" alt="Headphones" class="h-6 mr-2">Music
+</a>
 
-  onMount(async () => {
-    const darkReaderMetaTag = document.querySelector('meta[name="darkreader"]');
-    if (darkReaderMetaTag) {
-      alert('You\'re using darkreader, which breaks the styles on this page. Please disable it. :)');
-    }
-  });
-</script>
-
-
+  
+</body>
+</html>
---
+++ svelte/src/routes/music/+page.svelte
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!-- svelte-ignore a11y-missing-attribute -->
+<html>
+<head>
+	<meta http-equiv="refresh" content="0; url=https://jbugel.xyz/music">
+    <meta charset="UTF-8">
+    <meta name="robots" content="noindex">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Redirecting...</title>
+</head>
+<body class="h-screen w-screen bg-cat-base">
+	<p class="text-cat-text">If you are not redirected automatically, <a href="https://jbugel.xyz/music">click here</a>.</p>
+</body>
+</html>
+
+
