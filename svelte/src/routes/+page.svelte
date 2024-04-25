77049e8f6eed88389e3f4090deb810e9b4bac180
jbugel-lol <jonasbugelnig+gh1@jbugel.xyz>
2024-04-25 21:28:30 +0200

    animation, metatags, prettier

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,31 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta property="og:type" content="website">
-  <meta property="og:title" content="JBugel's Website">
-  <meta property="og:url" content="https://jbugel.xyz">
-  <meta property="og:description" content="Discover my projects, profile and more">
-  <meta property="og:image" content="https://example.com/images/website-preview.png">
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:site" content="@jbugel">
-  <meta name="twitter:creator" content="@jbugel">
-  <meta name="twitter:title" content="JBugel's Website">
-  <meta name="twitter:description" content="Discover my projects, profile and more">
-  <meta name="twitter:image" content="https://example.com/images/website-preview.png">
-  <title>jbugel</title>
-</head>
 <body class="h-screen w-screen bg-cat-base">
+<div class="flex justify-end space-x-7 p-4">
 <a class="text-cat-text flex items-center" href="/projects">
-  <img src="/file-folder.png" alt="Headphones" class="h-6 mr-2">Projects
-</a><a class="text-cat-text flex items-center" href="/contact">
-  <img src="/envelope.png" alt="Headphones" class="h-6 mr-2">Contact Me
+  <img src="/file-folder.png" alt="Headphones" class="h-6 mr-2 transition-transform duration-200 ease-out hover:transform-gpu hover:scale-125">Projects
 </a>
 <a class="text-cat-text flex items-center" href="/music">
-  <img src="/headphones.png" alt="Headphones" class="h-6 mr-2">Music
+  <img src="/headphones.png" alt="Headphones" class="h-6 mr-2 transition-transform duration-200 ease-out hover:transform-gpu hover:scale-125">Music
 </a>
-
-  
+<a class="text-cat-text flex items-center" href="/contact">
+  <img src="/envelope.png" alt="Headphones" class="h-6 mr-2 transition-transform duration-200 ease-out hover:transform-gpu hover:scale-125">Contact Me
+</a>
+</div>
 </body>
 </html>
