11cf5c7c190715c4190888dfbb787e86706adaef
jbugel <115107409+jbugel-lol@users.noreply.github.com>
2024-04-28 00:24:55 +0200

    Delete non-svelte directory

--- non-svelte/src/script/currentYear.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-	const yearSpan = document.getElementById("current-year");
-	if (yearSpan) {
-		yearSpan.textContent = new Date().getFullYear();
-	}
-});
--- non-svelte/src/script/darkReaderDetection.js
+++
@@ -1,5 +0,0 @@
-if (document.querySelector('meta[name="darkreader"]')) {
-  alert(
-    "You're using darkreader, which breaks the styles on this page. Please disable it. :)"
-  );
-}
