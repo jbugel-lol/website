c6890ad18b1516c48c6a50ee9f09ec28efd56bd2
jbugel-lol <jonasbugelnig+gh1@jbugel.xyz>
2024-05-08 17:06:39 +0200

    fixes

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <!-- svelte-ignore a11y-missing-attribute -->
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="robots" content="noindex">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Contact me</title>
-</head>
-<body class="h-screen w-screen bg-cat-base">
-	<p class="text-cat-text">Comming soon</p>
-</body>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="robots" content="noindex" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Contact me</title>
+	</head>
+	<body class="h-screen w-screen bg-cat-base">
+		<p class="text-cat-text">Coming soon</p>
+	</body>
 </html>
-
-
