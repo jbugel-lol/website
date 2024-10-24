a97ec0d92f657f24e5b00bb44ac19b0ef323001f
jbugel <115107409+jbugel-lol@users.noreply.github.com>
2024-10-24 20:39:03 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,20 @@
 			</div>
 			<div class="flex items-center">
 				<p class="text-3xl text-cat-text mr-4">jbugel - Portfolio</p>
+<div role="alert" class="alert alert-info">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    class="h-6 w-6 shrink-0 stroke-current">
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+  </svg>
+  <span>Website v3 is in progress</span>
+</div>
 			</div>
 			<div class="dropdown dropdown-hover">
 				<button
